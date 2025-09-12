5a775fc3453cc845e1372d24ed28538d6f159c71
rishinduyohan <rishinduyohan@gmail.com>
2025-09-12 23:21:12 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from settings import *
+import moderngl as mgl
+import pygame as pg
+import sys
+
+class Minecraft:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def render(self):
+        pass
+    def handle_events(self):
+        pass
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    app = Minecraft()
+    app.run()
