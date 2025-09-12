10ce26bb5c830ce38cd7c2fc0fda5b929ef8a01c
rishinduyohan <rishinduyohan@gmail.com>
2025-09-12 23:39:14 +0530

    check runtime and create garbage collection mode

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ class Minecraft:
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,pg.GL_CONTEXT_PROFILE_CORE)
         pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE,24)
 
+        pg.display.set_mode(WIN_RES,flags=pg.OPENGL | pg.DOUBLEBUF)
+        self.ctx = mgl.create_context()
+
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
+        self.ctx.gc_mode = 'auto'
+
+        self.clock = pg.time.Clock()
+        self.delta_time = 0
+        self.time = 0
+
+        self.is_running = True
+
     def update(self):
         pass
     def render(self):
