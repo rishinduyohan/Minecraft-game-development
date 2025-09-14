b0cb74e340cc2ebe990eb79e378bc56080191794
rishinduyohan <rishinduyohan@gmail.com>
2025-09-14 18:24:04 +0530

    Set colors to vertex data

--- meshes/quad_mesh.py
+++ meshes/quad_mesh.py
@@ -9,3 +9,15 @@ class QuadMesh(BaseMesh):
         self.app = app
         self.ctx = app.ctx
         self.program = app.shadder_program.quad
+
+    def get_vertex_data(self):
+        vertices = [
+            (0.5,0.5,0.0),(-0.5,0.5,0.0),(-0.5,-0.5,0.0),
+            (0.5, 0.5, 0.0), (-0.5, -0.5, 0.0), (0.5, -0.5, 0.0)
+        ]
+        colors = [
+            (0,1,0),(1,0,0),(1,1,0),
+            (0,1,0),(1,1,0),(0,0,1)
+        ]
+        vertex_data = np.hstack([vertices, colors],dtype = 'float32')
+        return vertex_data
