59d0097d9f529b88e614ce2b81a25de39075a9ca
rishinduyohan <rishinduyohan@gmail.com>
2025-09-16 19:00:00 +0530

    create a method to get shader program to vao

--- meshes/base_mesh.py
+++ meshes/base_mesh.py
@@ -15,4 +15,11 @@ class BaseMesh:
 
     def get_vertex_data(self) -> np.array: ...
 
+    def get_vao(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        vao = self.ctx.vertex_array(
+            self.program,[(vbo, self.vbo_format, *self.attrs)], skip_errors=True
+        )
+        return vao
 
