bf7178f9ef3ca7b012375ea649414a27fc19dcfa
rishinduyohan <rishinduyohan@gmail.com>
2025-09-16 18:54:45 +0530

    Recreate openGL context

--- meshes/base_mesh.py
+++ meshes/base_mesh.py
@@ -2,26 +2,13 @@ import numpy as np
 
 class BaseMesh:
     def __init__(self):
-        #OpenGL
+        #OpenGL context
         self.ctx = None
         #Shader program
         self.program = None
         #vertex buffer data type format: "3f 3f"
         self.vbo_format = None
-        #attribute names ("in_position", "in_color")
-        self.attrs: tuple[str,...] = None
+        #attribute names according to the format:("in_position","in_color")
+        self.attrs : tuple[str,...] = None
         #vertex array object
-        self.vbo = None
-
-    def get_vertex_array(self) -> np.array:...
-
-    def get_vao(self):
-        vertex_data = self.get_vertex_array()
-        vbo = self.ctx.buffer(vertex_data)
-        vao = self.ctx.vertex_array(
-            self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors=True
-        )
-        return vao
-
-    def render(self):
-        self.vbo.render()
+        self.vao = None
