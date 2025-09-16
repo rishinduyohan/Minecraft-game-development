73849268df996fd9b7a6ff98b969bba4862a7354
rishinduyohan <rishinduyohan@gmail.com>
2025-09-16 18:41:56 +0530

    create get program function

--- shader_program.py
+++ shader_program.py
@@ -5,3 +5,12 @@ class ShaderProgram:
         self.app = app
         self.ctx = app.ctx
 
+    def get_program(self,shader_name):
+        with open(f'shaders/{shader_name}.vert') as file:
+            vertex_shader = file.read()
+
+        with open(f'shaders/{shader_name}.frag') as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+        return program
