0e9a0d8a3e409cbb2aa984286943bdb74785b5cb
rishinduyohan <rishinduyohan@gmail.com>
2025-09-17 20:55:12 +0530

    Create player from camera class

---
+++ player.py
@@ -0,0 +1,10 @@
+import pygame as pg
+from camera import Camera
+from settings import *
+
+
+class Player(Camera):
+    def __init__(self, app, position=PLAYER_POS, yaw=-90, pitch=0):
+        self.app = app
+        super().__init__(position,yaw,pitch)
+
