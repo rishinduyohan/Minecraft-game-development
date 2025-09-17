f29777ca537e7eeab49880ea687c58cf822c6bd5
rishinduyohan <rishinduyohan@gmail.com>
2025-09-17 20:10:22 +0530

    create camera functions in setting file

--- settings.py
+++ settings.py
@@ -6,5 +6,14 @@ import math
 #resolution
 WIN_RES = (800, 600)
 
+#camera
+ASPECT_RATIO = WIN_RES.x / WIN_RES.y
+FOV_DEG = 50
+V_FOV = glm.radians(FOV_DEG)
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
+NEAR = 0.1
+FAR = 2000.0
+PITCH_MAX = glm.radians(89)
+
 #colors
 BG_COLOR = (0.1, 0.16, 0.25)
