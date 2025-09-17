8d03b02d2ffe49358a159517a36f2c152d8cc391
rishinduyohan <rishinduyohan@gmail.com>
2025-09-17 20:29:48 +0530

    create camera methods

---
+++ camera.py
@@ -0,0 +1,23 @@
+from settings import *
+
+class Camera:
+    def __init__(self, position, yaw, pitch):
+        self.position = glm.ve3(position)
+        self.yaw = glm.radians(yaw)
+        self.pitch = glm.radians(pitch)
+
+        self.up = glm.vec3(0, 1, 0)
+        self.right = glm.vec3(1, 0, 0)
+        self.forward = glm.vec3(0, 0, -1)
+
+        self.m_proj = glm.perspective(V_FOV, ASPECT_RATIO, NEAR, FAR)
+        self.m_proj = glm.mat4()
+
+    def update_vectors(self):
+        self.forward.x = glm.cos(self.yaw) * glm.cos(self.pitch)
+        self.forward.y = glm.sin(self.pitch)
+        self.forward.z = glm.sin(self.yaw) * glm.cos(self.pitch)
+
+        self.forward = glm.normalize(self.forward)
+        self.right = glm.normalize(glm.cross(self.forward, glm.vec3(0, 1, 0)))
+        self.up = glm.normalize(glm.cross(self.right, self.forward))
