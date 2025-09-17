fa2e9e0c40169a3bc48ffd0f328eb8d1111409d5
rishinduyohan <rishinduyohan@gmail.com>
2025-09-17 21:09:49 +0530

    Add player to main file and shader program

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame as pg
 import sys
 from shader_program import ShaderProgram
 from scene import Scene
+from player import Player
+
 
 class Minecraft:
     def __init__(self):
@@ -29,10 +31,12 @@ class Minecraft:
         self.on_init()
 
     def on_init(self):
+        self.player = Player(self)
         self.shader_program = ShaderProgram(self)
         self.scene = Scene(self)
 
     def update(self):
+        self.player.update()
         self.shader_program.update()
         self.scene.update()
 
--- shader_program.py
+++ shader_program.py
@@ -4,16 +4,18 @@ class ShaderProgram:
     def __init__(self,app):
         self.app = app
         self.ctx = app.ctx
+        self.player = app.player
         #----------- shaders ---------#
         self.quad = self.get_program(shader_name='quad')
         #------------------------------
         self.set_uniforms_on_init()
 
     def set_uniforms_on_init(self):
-        pass
+        self.quad['m_proj'].write(self.player.m_proj)
+        self.quad['m_model'].write(glm.mat4())
 
     def update(self):
-        pass
+        self.quad['m_view'].write(self.player.m_view)
 
     def get_program(self,shader_name):
         with open(f'shaders/{shader_name}.vert') as file:
