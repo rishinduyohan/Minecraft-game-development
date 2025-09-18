d11fc28aad9251cc04d8729b96fed3842409236c
rishinduyohan <rishinduyohan@gmail.com>
2025-09-18 21:35:37 +0530

    Update positions through settings.py using chunk functions

--- settings.py
+++ settings.py
@@ -6,8 +6,14 @@ import math
 #resolution
 WIN_RES = (800, 600)
 
+#chunk
+CHUNK_SIZE = 32
+H_CHUNK_SIZE = CHUNK_SIZE // 2
+CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE
+CHUNK_VOL = CHUNK_AREA * CHUNK_SIZE
+
 #camera
-ASPECT_RATIO = WIN_RES.x / WIN_RES.y
+ASPECT_RATIO: object = WIN_RES.x / WIN_RES.y
 FOV_DEG = 50
 V_FOV = glm.radians(FOV_DEG)
 H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
@@ -18,7 +24,7 @@ PITCH_MAX = glm.radians(89)
 # player
 PLAYER_SPEED = 0.005
 PLAYER_ROT_SPEED = 0.003
-PLAYER_POS = glm.vec3(0, 0, 1)
+PLAYER_POS = glm.vec3(H_CHUNK_SIZE, CHUNK_SIZE, 1.5 * CHUNK_SIZE)
 MOUSE_SENSITIVITY = 0.002
 
 #colors
